5d77c5d678a31877aa53ab974a3e524825d10de3
Mfalme Griffin <mfalmegriffin@gmail.com>
2023-05-03 20:16:30 +0300

    use react instead of nextjs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        This is my initiaal change for my first personal Reaact aapp
+      </h1>
+    </div>
+  );
+}
+
+export default App;
